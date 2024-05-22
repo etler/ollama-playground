eb0b8a324105925c5f7fe905653f738f625363a1
Tim Etler <tmetler@gmail.com>
2024-05-22 15:48:01 -0700

    Added test options
    Added openai as a provider

--- test.js
+++ test.js
@@ -1,12 +1,34 @@
+import "dotenv/config";
 import ollama from "ollama";
+import OpenAI from "openai";
 
-const content = `Convert the following string into a human readable search query in the format of "What is the best ____?". Do not output any additional text or formatting such as json or quotation marks. "tea kettle retro electric"`;
+const [platform, model, limitString, input] = process.argv.slice(2);
 
-for (let i = 0; i < 10; i++) {
-  const phiModelName = "phi3:mini";
-  const phiStart = performance.mark(`${phiModelName}:start`);
-  const phiResponse = await ollama.chat({
-    model: "phi3:mini",
+const openai = new OpenAI({
+  apiKey: process.env["OPENAI_API_KEY"],
+});
+
+const content =
+  input ??
+  `Convert the following string into a human readable search query in the format of "What is the best ____?". Do not output any additional text or formatting such as json or quotation marks. "tea kettle retro electric"`;
+
+const limit = limitString ? parseInt(limitString) : 10;
+
+const addMeasure = (func) => async () => {
+  const startMark = performance.mark(`${model}:start`);
+  const result = await func();
+  const endMark = performance.mark(`${model}:end`);
+  console.log(`${model}: ${result}`);
+  console.log(
+    `Time Elapsed: ${
+      performance.measure(model, startMark.name, endMark.name).duration
+    }ms`
+  );
+};
+
+const testOllama = async () => {
+  const response = await ollama.chat({
+    model,
     messages: [
       {
         role: "user",
@@ -14,33 +36,33 @@ for (let i = 0; i < 10; i++) {
       },
     ],
   });
-  const phiEnd = performance.mark(`${phiModelName}:end`);
-  console.log(`Phi: ${phiResponse.message.content}`);
-  console.log(
-    `Time Elapsed: ${
-      performance.measure(phiModelName, phiStart.name, phiEnd.name).duration
-    }ms`
-  );
+  return response.message.content;
+};
+
+const testOpenai = async () => {
+  const response = await openai.chat.completions.create({
+    messages: [{ role: "user", content }],
+    model,
+  });
+  return response.choices[0].message.content;
+};
+
+const test = (() => {
+  switch (platform) {
+    case "ollama":
+      return addMeasure(testOllama);
+    case "openai":
+      return addMeasure(testOpenai);
+    default:
+      return null;
+  }
+})();
+
+if (test === null) {
+  console.log("Invalid platform");
+  process.exit(1);
 }
 
-for (let i = 0; i < 10; i++) {
-  const gemmaModelName = "phi3:mini";
-  const gemmaStart = performance.mark(`${gemmaModelName}:start`);
-  const gemmaResponse = await ollama.chat({
-    model: "gemma:2b",
-    messages: [
-      {
-        role: "user",
-        content,
-      },
-    ],
-  });
-  const gemmaEnd = performance.mark(`${gemmaModelName}:end`);
-  console.log(`Gemma: ${gemmaResponse.message.content}`);
-  console.log(
-    `Time Elapsed: ${
-      performance.measure(gemmaModelName, gemmaStart.name, gemmaEnd.name)
-        .duration
-    }ms`
-  );
+for (let i = 0; i < limit; i++) {
+  test();
 }
