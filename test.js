3e11a00c423ea87f2639e960a7ca0e840141e656
Tim Etler <tmetler@gmail.com>
2024-05-22 14:49:32 -0700

    Added basic ollama tests

---
+++ test.js
@@ -0,0 +1,41 @@
+import ollama from "ollama";
+
+const content = `Convert the following string into a human readable search query in the format of "What is the best ____?". Do not output any additional text or formatting such as json or quotation marks. "tea kettle retro electric"`;
+
+const phiModelName = "phi3:mini";
+const phiStart = performance.mark(`${phiModelName}:start`);
+const phiResponse = await ollama.chat({
+  model: "phi3:mini",
+  messages: [
+    {
+      role: "user",
+      content,
+    },
+  ],
+});
+const phiEnd = performance.mark(`${phiModelName}:end`);
+console.log(`Phi: ${phiResponse.message.content}`);
+console.log(
+  `Time Elapsed: ${
+    performance.measure(phiModelName, phiStart.name, phiEnd.name).duration
+  }ms`
+);
+
+const gemmaModelName = "phi3:mini";
+const gemmaStart = performance.mark(`${gemmaModelName}:start`);
+const gemmaResponse = await ollama.chat({
+  model: "gemma:2b",
+  messages: [
+    {
+      role: "user",
+      content,
+    },
+  ],
+});
+const gemmaEnd = performance.mark(`${gemmaModelName}:end`);
+console.log(`Gemma: ${gemmaResponse.message.content}`);
+console.log(
+  `Time Elapsed: ${
+    performance.measure(gemmaModelName, gemmaStart.name, gemmaEnd.name).duration
+  }ms`
+);
